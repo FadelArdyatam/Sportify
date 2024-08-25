0e966b5498d5cfe72afe10f26f01203767fdc7a7
FadelArdyatam <155771319+FadelArdyatam@users.noreply.github.com>
2024-08-25 13:27:55 +0700

    fadel update

--- src/Components/Marquee.jsx
+++ src/Components/Marquee.jsx
@@ -2,10 +2,7 @@ export function Marquee({ className }) {
     return (
       <div className={`mt-10 overflow-hidden  flex bg-primary dark:bg-white dark:text-black text-black font-sans text-6xl font-bold font ${className}`}>
         <span className="relative shrink-0 flex justify-around min-w-full animate-marquee">
-            SWEAT NOW, SHINE LATER
-        </span>
-        <span className="relative shrink-0 flex justify-around min-w-full animate-marquee">
-            YOUR JOURNEY STARTS HERE!
+            SWEAT NOW, SHINE LATER                             YOUR JOURNEY STARTS HERE!
         </span>
       </div>
     );
